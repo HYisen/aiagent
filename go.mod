28936083b64d4910ddc6c106abf80ad2b22234ac
Alex <alexhyisen@gmail.com>
2025-03-02 21:15:37 +0800

    add OneShotStream demo

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module aiagent
 go 1.24
 
 require (
-	github.com/openai/openai-go v0.1.0-alpha.59 // indirect
+	github.com/openai/openai-go v0.1.0-alpha.61 // indirect
 	github.com/tidwall/gjson v1.18.0 // indirect
 	github.com/tidwall/match v1.1.1 // indirect
 	github.com/tidwall/pretty v1.2.1 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 	"encoding/json"
+	"errors"
 	"flag"
 	"fmt"
 	"github.com/openai/openai-go"
@@ -23,11 +24,9 @@ var DeepSeekAPIKey = flag.String("DeepSeekAPIKey", "this_is_a_secret", "API Key 
 
 func main() {
 	service := NewService(NewDeepSeekClient(*DeepSeekAPIKey))
-	msg, err := service.OneShot("write a haiku about ai")
-	if err != nil {
+	if err := service.OneShotStream("say this is a test"); err != nil {
 		log.Fatalln(err)
 	}
-	msg.Print()
 }
 
 type Service struct {
@@ -74,6 +73,54 @@ func (s *Service) OneShot(content string) (*Message, error) {
 		return nil, fmt.Errorf("parse JSON in response: %w", err)
 	}
 	return &msg, nil
+}
+
+func (s *Service) OneShotStream(content string) error {
+	stream := s.client.Chat.Completions.NewStreaming(context.TODO(), openai.ChatCompletionNewParams{
+		Messages: openai.F([]openai.ChatCompletionMessageParamUnion{
+			openai.UserMessage(content),
+		}),
+		Model: openai.F(ChatModelDeepSeekR1),
+	})
+
+	acc := openai.ChatCompletionAccumulator{}
+	for stream.Next() {
+		chunk := stream.Current()
+		acc.AddChunk(chunk)
+		//log.Printf("%+v\n", chunk)
+
+		var msg Message
+		if err := json.Unmarshal([]byte(chunk.Choices[0].Delta.JSON.RawJSON()), &msg); err != nil {
+			return fmt.Errorf("parse JSON in response [%v]: %v", chunk.JSON.RawJSON(), err)
+		}
+		if msg.Role != "" {
+			fmt.Printf("role: %s\n", msg.Role)
+		}
+		if msg.ReasoningContent != "" {
+			fmt.Printf("%s", msg.ReasoningContent)
+		}
+		if msg.Content != "" {
+			fmt.Print(msg.Content)
+		}
+
+		if content, ok := acc.JustFinishedContent(); ok {
+			fmt.Printf("JustFinishedContent: %v\n", content)
+		}
+
+		if _, ok := acc.JustFinishedToolCall(); ok {
+			return fmt.Errorf("handle JustFinishedToolCall in stream: %w", errors.ErrUnsupported)
+		}
+
+		if refusal, ok := acc.JustFinishedRefusal(); ok {
+			fmt.Printf("JustFinishedRefusal: %v\n", refusal)
+		}
+
+		//if len(chunk.Choices) > 0 {
+		//	fmt.Println("> " + chunk.Choices[0].Delta.Content)
+		//}
+	}
+
+	return stream.Err()
 }
 
 type Message struct {
