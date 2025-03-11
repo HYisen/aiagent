b302ed9d6437b3da33b354a8603dc49d0cd627da
Alex <alexhyisen@gmail.com>
2025-03-12 02:54:23 +0800

    add v1PostSessionChatStream

--- README.md
+++ README.md
@@ -35,3 +35,23 @@ Most likely there is something wrong with the response parser.
 
 I don't want to fork the client to dig out how and fix it, but
 decided to maintain a minimum one of top quality myself.
+
+### Why not Server-Sent Events third-party package?
+
+Because it's easy.
+
+First I took a glance over third-party library such as
+[sse](https://github.com/r3labs/sse)
+and
+[go-sse](https://github.com/tmaxmax/go-sse)
+.
+
+And then figure out that the official client maintain the codec by themselves.
+[usage](https://github.com/openai/openai-go/blob/main/chatcompletion.go#L150)
+&
+[implementation](https://github.com/openai/openai-go/blob/main/packages/ssestream/ssestream.go)
+
+Later I took [a tutorial](https://www.freecodecamp.org/news/how-to-implement-server-sent-events-in-go/) as an example.
+Finding that it might be much easier if I maintain a codec by myself.
+
+As I only need a narrow range of features, can drop compatibility and generic as long as it works in my case.
--- service/web.go
+++ service/web.go
@@ -8,11 +8,13 @@ import (
 	"context"
 	"encoding/json"
 	"errors"
+	"fmt"
 	"github.com/hyisen/wf"
 	"gorm.io/gorm"
 	"log/slog"
 	"net/http"
 	"reflect"
+	"strings"
 	"time"
 )
 
@@ -68,27 +70,9 @@ type ChatRequest struct {
 }
 
 func (s *Service) Chat(ctx context.Context, id int, req *ChatRequest) (*openai.ChatCompletion, *wf.CodedError) {
-	ses, err := s.sessionRepository.FindWithChats(ctx, id)
-	if errors.Is(err, gorm.ErrRecordNotFound) {
-		return nil, wf.NewCodedErrorf(http.StatusNotFound, "no session on id %v to chat", id)
-	}
-	if err != nil {
-		return nil, wf.NewCodedError(http.StatusInternalServerError, err)
-	}
-
-	if err := s.chatRepository.Save(ctx, &model.Chat{
-		ID:         0,
-		SessionID:  ses.ID,
-		Input:      req.Content,
-		CreateTime: time.Now().UnixMilli(),
-		Result:     nil,
-	}); err != nil {
-		return nil, wf.NewCodedError(http.StatusInternalServerError, err)
-	}
-
-	neo, err := s.chatRepository.FindLastBySessionID(ctx, ses.ID)
-	if err != nil {
-		return nil, wf.NewCodedError(http.StatusInternalServerError, err)
+	ses, neo, e := s.prepareChat(ctx, id, req)
+	if e != nil {
+		return nil, e
 	}
 
 	chatCompletion, err := s.client.OneShot(ctx, openai.Request{
@@ -105,6 +89,139 @@ func (s *Service) Chat(ctx context.Context, id int, req *ChatRequest) (*openai.C
 		return nil, wf.NewCodedError(http.StatusInternalServerError, err)
 	}
 	return chatCompletion, nil
+}
+
+func (s *Service) prepareChat(ctx context.Context, id int, req *ChatRequest) (
+	ses *model.Session,
+	neo *model.Chat,
+	err *wf.CodedError,
+) {
+	ses, e := s.sessionRepository.FindWithChats(ctx, id)
+	if errors.Is(e, gorm.ErrRecordNotFound) {
+		return nil, nil, wf.NewCodedErrorf(http.StatusNotFound, "no session on id %v to chat", id)
+	}
+	if e != nil {
+		return nil, nil, wf.NewCodedError(http.StatusInternalServerError, err)
+	}
+
+	if err := s.chatRepository.Save(ctx, &model.Chat{
+		ID:         0,
+		SessionID:  ses.ID,
+		Input:      req.Content,
+		CreateTime: time.Now().UnixMilli(),
+		Result:     nil,
+	}); err != nil {
+		return nil, nil, wf.NewCodedError(http.StatusInternalServerError, err)
+	}
+
+	neo, e = s.chatRepository.FindLastBySessionID(ctx, ses.ID)
+	if e != nil {
+		return nil, nil, wf.NewCodedError(http.StatusInternalServerError, err)
+	}
+
+	return ses, neo, nil
+}
+
+func (s *Service) ChatStream(ctx context.Context, id int, req *ChatRequest) (<-chan wf.MessageEvent, *wf.CodedError) {
+	ses, neo, e := s.prepareChat(ctx, id, req)
+	if e != nil {
+		return nil, e
+	}
+
+	up, err := s.client.OneShotStreamFast(ctx, openai.Request{
+		Messages: append(ses.History(), openai.NewUserMessage(req.Content)),
+		Model:    req.Model,
+	})
+	if err != nil {
+		return nil, wf.NewCodedErrorf(http.StatusInternalServerError, "upstream: %v", err.Error())
+	}
+
+	down := make(chan wf.MessageEvent)
+	go s.translateAggregateSave(ctx, up, down, neo)
+	return down, nil
+}
+
+func (s *Service) translateAggregateSave(
+	ctx context.Context,
+	up <-chan openai.ChatCompletionChunk,
+	down chan<- wf.MessageEvent,
+	neo *model.Chat,
+) {
+	defer close(down)
+	aggregator := openai.NewAggregator()
+	var stage int // 0head 1ReasoningContent 2Content 3tail
+	for chunk := range up {
+		aggregator.Aggregate(chunk)
+		switch stage {
+		case 0:
+			stage++
+			down <- NewJSONMessageEvent("head", chunk.ChatCompletionBase)
+			down <- wf.MessageEvent{
+				TypeOptional: "role",
+				Lines:        []string{chunk.Choices[0].Delta.Role},
+			}
+		case 1:
+			if chunk.Choices[0].Delta.Content == "" {
+				down <- NewMultiLineMessageEvent(chunk.Choices[0].Delta.ReasoningContent)
+			} else {
+				stage++
+				down <- wf.MessageEvent{
+					TypeOptional: "cotEnd",
+					Lines:        nil,
+				}
+				down <- NewMultiLineMessageEvent(chunk.Choices[0].Delta.Content)
+			}
+		case 2:
+			if chunk.Usage == nil {
+				down <- NewMultiLineMessageEvent(chunk.Choices[0].Delta.Content)
+			} else {
+				stage++
+				down <- wf.MessageEvent{
+					TypeOptional: "finish",
+					Lines:        []string{*chunk.Choices[0].FinishReason},
+				}
+				down <- NewJSONMessageEvent("usage", chunk.Usage)
+			}
+		}
+	}
+	if stage != 3 {
+		slog.Error("end with unexpected status", "stage", stage)
+	}
+	neo.Result = model.NewResult(aggregator)
+	if err := s.chatRepository.Save(ctx, neo); err != nil {
+		slog.Error("can not append record in stream mode", "chat", neo)
+		down <- NewErrorMessageEvent(err)
+	}
+}
+
+func NewMultiLineMessageEvent(passage string) wf.MessageEvent {
+	return wf.MessageEvent{
+		TypeOptional: "",
+		// One single LF would become 2 data: with empty value, build to 1 LF again in client.
+		Lines: strings.Split(passage, "\n"),
+	}
+}
+
+func NewErrorMessageEvent(e error) wf.MessageEvent {
+	return wf.MessageEvent{
+		TypeOptional: "error",
+		Lines:        strings.Split(e.Error(), "\n"),
+	}
+}
+
+// NewJSONMessageEvent marshall item to JSON string, put alone with typeOptional to the returned value.
+// If fails, log err and NewErrorMessageEvent invoked with err would be returned.
+func NewJSONMessageEvent(typeOptional string, item any) wf.MessageEvent {
+	data, err := json.Marshal(item)
+	if err != nil {
+		slog.Error("NewJSONMessageEvent encode", "err", err, "item", item)
+		return NewErrorMessageEvent(fmt.Errorf("parse item %+v to JSON: %w", item, err))
+	}
+	return wf.MessageEvent{
+		TypeOptional: typeOptional,
+		// JSON marshaller escape LF, TestJSONEscapeLine assert that. No split by line needed here.
+		Lines: []string{string(data)},
+	}
 }
 
 func New(
@@ -148,24 +265,43 @@ func New(
 	v1PostSessionChatPathSuffix := "/chat"
 	v1PostSessionChatPathIDParser := wf.PathIDParser(v1PostSessionChatPathSuffix)
 	v1PostSessionChatPayloadParser := wf.JSONParser(reflect.TypeOf(ChatRequest{}))
+	v1PostSessionChatMatcher := wf.ResourceWithID(http.MethodPost, "/v1/sessions/", v1PostSessionChatPathSuffix)
+	v1PostSessionChatParser := func(data []byte, path string) (any, error) {
+		id, err := v1PostSessionChatPathIDParser(nil, path)
+		if err != nil {
+			return nil, err
+		}
+		payload, err := v1PostSessionChatPayloadParser(data, "")
+		if err != nil {
+			return nil, err
+		}
+		return []any{id, payload}, nil
+	}
 	v1PostSessionChat := wf.NewClosureHandler(
-		wf.ResourceWithID(http.MethodPost, "/v1/sessions/", v1PostSessionChatPathSuffix),
-		func(data []byte, path string) (any, error) {
-			id, err := v1PostSessionChatPathIDParser(nil, path)
-			if err != nil {
-				return nil, err
+		func(req *http.Request) bool {
+			if !v1PostSessionChatMatcher(req) {
+				return false
 			}
-			payload, err := v1PostSessionChatPayloadParser(data, "")
-			if err != nil {
-				return nil, err
-			}
-			return []any{id, payload}, nil
+			return req.URL.Query().Get("stream") != "true"
 		},
+		v1PostSessionChatParser,
 		func(ctx context.Context, req any) (rsp any, codedError *wf.CodedError) {
 			return ret.Chat(ctx, req.([]any)[0].(int), req.([]any)[1].(*ChatRequest))
 		},
 		json.Marshal,
 		wf.JSONContentType,
+	)
+	v1PostSessionChatStream := wf.NewServerSentEventsHandler(
+		func(req *http.Request) bool {
+			if !v1PostSessionChatMatcher(req) {
+				return false
+			}
+			return req.URL.Query().Get("stream") == "true"
+		},
+		v1PostSessionChatParser,
+		func(ctx context.Context, req any) (ch <-chan wf.MessageEvent, codedError *wf.CodedError) {
+			return ret.ChatStream(ctx, req.([]any)[0].(int), req.([]any)[1].(*ChatRequest))
+		},
 	)
 
 	ret.web = wf.NewWeb(
@@ -174,6 +310,7 @@ func New(
 		v1GetSessions,
 		v1GetSessionByID,
 		v1PostSessionChat,
+		v1PostSessionChatStream,
 	)
 	return ret
 }
