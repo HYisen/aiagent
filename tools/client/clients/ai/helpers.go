8cbf7a570428113669cdbed10544edc2578e0337
Alex <alexhyisen@gmail.com>
2026-05-17 00:34:15 +0800

    client: inline VerifyStatusReadBodyAll and doRequestAndVerifyStatusReadBodyAll

--- tools/client/clients/ai/helpers.go
+++ tools/client/clients/ai/helpers.go
@@ -10,38 +10,28 @@ import (
 
 func FetchAndParseJSON[ResponseType any](request *http.Request) (ResponseType, error) {
 	var ret ResponseType
-	data, err := doRequestAndVerifyStatusReadBodyAll(request)
+
+	resp, err := http.DefaultClient.Do(request)
 	if err != nil {
 		return ret, err
+	}
+	defer func(c io.Closer) {
+		err = errors.Join(err, c.Close())
+	}(resp.Body)
+
+	data, err := io.ReadAll(resp.Body)
+	if err != nil {
+		return ret, err
+	}
+	if resp.StatusCode == http.StatusForbidden {
+		return ret, fmt.Errorf("%w: %s", ErrForbidden, string(data))
+	}
+	if resp.StatusCode != http.StatusOK {
+		return ret, fmt.Errorf("unexpected response status code %d: %s", resp.StatusCode, string(data))
 	}
 
 	if err := json.Unmarshal(data, &ret); err != nil {
 		return ret, err
 	}
 	return ret, nil
-}
-
-func doRequestAndVerifyStatusReadBodyAll(req *http.Request) (responsePayload []byte, err error) {
-	resp, err := http.DefaultClient.Do(req)
-	if err != nil {
-		return nil, err
-	}
-	defer func(c io.Closer) {
-		err = errors.Join(err, c.Close())
-	}(resp.Body)
-	return VerifyStatusReadBodyAll(resp)
-}
-
-func VerifyStatusReadBodyAll(resp *http.Response) (responsePayload []byte, err error) {
-	data, err := io.ReadAll(resp.Body)
-	if err != nil {
-		return nil, err
-	}
-	if resp.StatusCode == http.StatusForbidden {
-		return nil, fmt.Errorf("%w: %s", ErrForbidden, string(data))
-	}
-	if resp.StatusCode != http.StatusOK {
-		return nil, fmt.Errorf("unexpected response status code %d: %s", resp.StatusCode, string(data))
-	}
-	return data, nil
 }
